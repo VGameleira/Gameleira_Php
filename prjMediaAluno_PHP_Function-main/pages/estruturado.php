f49b5f0cf8040841ce58bc704a41675013402ffa
VGameleira.Dev <vinigameleira.stos@gmail.com>
2025-10-01 17:28:34 -0300

    prjMediaFunction
    Continuação da Atividade

--- prjMediaAluno_PHP_Function-main/pages/estruturado.php
+++ prjMediaAluno_PHP_Function-main/pages/estruturado.php
@@ -20,3 +20,23 @@ function MensagemResultado(float $media): string {
     }
 }
 
+// function validarEntrada deve ter as seguintes condições:
+// - nome deve estar definido e não ser vazio
+// - notas deve estar definido, ser um array e ter pelo menos um elemento
+// - itens do array notas devem ser numéricos e entre 0 e 10
+// - array não pode ser vazio
+// - se alguma dessas condições não for atendida, redirecionar para index.html
+
+
+function validarEntrada($nome, $notas) {
+    if (isset($nome) && !empty($nome) && isset($notas) && is_array($notas) && count($notas) > 0) {
+        foreach ($notas as $nota) {
+            if (!is_numeric($nota) || $nota < 0 || $nota > 10) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return false;
+    }
+}
